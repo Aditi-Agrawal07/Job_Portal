4926e575db38f2d0b3e2ae8fc8360646f1818bb5
Aditi-Agrawal-07 <07aditiagrawal@gmail.com>
2024-01-06 14:36:18 +0530

    solve error

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 // Import Utilities
-const  { database } = require("./utils") 
+// const  { database } = require("./utils") 
 
 // Start Routes
-const { userRouter,jobRouter,studentRouter  } = require("./Routes")
+// const { userRouter,jobRouter,studentRouter  } = require("./Routes")
 
-app.use("/user", userRouter)
-app.use("/jobs", jobRouter)
-app.use("/student",studentRouter)
+// app.use("/user", userRouter)
+// app.use("/jobs", jobRouter)
+// app.use("/student",studentRouter)
 
 module.exports = app
